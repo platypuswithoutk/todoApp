85171901d3a24e63fe34edadda96237786580e0c
Roksana Dzioba <roksanadzioba@gmail.com>
2020-09-16 17:09:24 +0200

    Updates

---
+++ precache-manifest.7259328eafe7dc9e5c90ffb6c95bdd0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a2e12d5f1d3f938a75e4105c23ac1b",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "0e6cc729e9078b18efb5",
+    "url": "/todoApp/static/css/main.50a780c8.chunk.css"
+  },
+  {
+    "revision": "0973f781dbeb785b68a2",
+    "url": "/todoApp/static/js/2.7526125e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.7526125e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6cc729e9078b18efb5",
+    "url": "/todoApp/static/js/main.e39ed21c.chunk.js"
+  },
+  {
+    "revision": "8d72b9e18fd9ba1ca521",
+    "url": "/todoApp/static/js/runtime-main.46a836fe.js"
+  }
+]);
